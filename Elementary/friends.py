5405cfe99c892c61c4206c022a6a767e3e8e7be1
Dani <Dani4kor@gmail.com>
2016-07-17 02:27:37 +0300

    friends - the final phase

---
+++ Elementary/friends.py
@@ -0,0 +1,48 @@
+class Friends:
+    def __init__(self, connections):
+        self.connections = list(connections)
+
+    def add(self, connection):
+        if connection in self.connections:
+            return False
+        else:
+            self.connections.append(connection)
+            return True
+
+    def remove(self, connection):
+        if connection in self.connections:
+            self.connections.remove(connection)
+            return True
+        return False
+
+    def names(self):
+        q = set()
+        for i in self.connections:
+            q = self.connections[0] | i
+        return q
+
+    def connected(self, name):
+        qwer = set()
+        qwer.add(name)
+        l =[]
+        print qwer
+       # for i in self.connections:
+            #if name in i:
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    letter_friends = Friends(({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"}))
+    digit_friends = Friends([{"1", "2"}, {"3", "1"}])
+    assert letter_friends.add({"c", "d"}) is True, "Add"
+    assert letter_friends.add({"c", "d"}) is False, "Add again"
+    assert letter_friends.remove({"c", "d"}) is True, "Remove"
+    assert digit_friends.remove({"c", "d"}) is False, "Remove non exists"
+    assert letter_friends.names() == {"a", "b", "c"}, "Names"
+    assert letter_friends.connected("d") == set(), "Non connected name"
+    assert letter_friends.connected("a") == {"b", "c"}, "Connected name"
