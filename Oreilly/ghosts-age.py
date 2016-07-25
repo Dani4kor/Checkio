2e62f9c2d341cf5ff340da093840d876becb0053
Dani <Dani4kor@gmail.com>
2016-07-25 22:21:55 +0300

    add ghost age solution

--- Oreilly/ghosts-age.py
+++ Oreilly/ghosts-age.py
@@ -1,10 +1,3 @@
-from math import sqrt
-
-
-def f(n):
-    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
-
-
 def fib(n):
     result = []
     a, b = 0, 1
@@ -15,10 +8,17 @@ def fib(n):
 
 
 def checkio(opacity):
-    print fib(10)
+    opp = 10000
+    age = 0
+    while opp != opacity:
+        age += 1
+        if age in fib(5000):
+            opp = opp - age
+        else:
+            opp += 1
+    return age
 
-
-#These "asserts" using only for self-checking and not necessary for auto-testing
+# These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     assert checkio(10000) == 0, "Newborn"
     assert checkio(9999) == 1, "1 year"
