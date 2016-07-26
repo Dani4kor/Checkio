ee55248c9fd501bea2912b8bd0fef99c605a1c2d
Dani <Dani4kor@gmail.com>
2016-07-26 06:43:26 +0300

    fix PEP8

--- Codeship/ghost-detect.py
+++ Codeship/ghost-detect.py
@@ -1,3 +1,10 @@
 def recognize(number):
     import re
     return len(set(re.findall('1+', bin(number)[2:]))) == 1
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert recognize(21) == True, "First example"
+    assert recognize(1587) == True, "Second example"
+    assert recognize(3687) == False, "Thid example"
