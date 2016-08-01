2fe96047227c8ed3399d8bb90a163bfa7124859f
Dani <Dani4kor@gmail.com>
2016-08-01 23:48:28 +0300

    add preDFS code

--- Oreilly/disposable-teleports.py
+++ Oreilly/disposable-teleports.py
@@ -1,7 +1,23 @@
 def checkio(teleports_string):
-    #return any route from 1 to 1 over all points
+    route = ['1']
+    node = dict()
+    for i in teleports_string.split(','):
+        node.setdefault(i[0], []).append(i[1]), node.setdefault(i[1], []).append(i[0])
+    print node
 
-    return "123456781"
+    pre = 0
+    current = route[-1]
+    open2 = []
+    for x in node[current]:
+        print x
+        if x > pre:
+            open2.append(x)
+    print open2
+
+
+
+
+
 
 #This part is using only for self-testing
 if __name__ == "__main__":
@@ -26,6 +42,6 @@ if __name__ == "__main__":
         return True
 
     assert check_solution(checkio, "12,23,34,45,56,67,78,81"), "First"
-    assert check_solution(checkio, "12,28,87,71,13,14,34,35,45,46,63,65"), "Second"
-    assert check_solution(checkio, "12,15,16,23,24,28,83,85,86,87,71,74,56"), "Third"
-    assert check_solution(checkio, "13,14,23,25,34,35,47,56,58,76,68"), "Fourth"
+    #assert check_solution(checkio, "12,28,87,71,13,14,34,35,45,46,63,65"), "Second"
+    #assert check_solution(checkio, "12,15,16,23,24,28,83,85,86,87,71,74,56"), "Third"
+    #assert check_solution(checkio, "13,14,23,25,34,35,47,56,58,76,68"), "Fourth"
