c263faf9876a2d3eb7dc0e2db61befdd45663cae
Dani <Dani4kor@gmail.com>
2016-08-05 06:36:37 +0300

    dsip-teleport solution

--- Oreilly/disposable-teleports.py
+++ Oreilly/disposable-teleports.py
@@ -1,20 +1,57 @@
+# def dfs(graph, start):
+#     visited, stack = [], [start]
+#     poop = graph
+#     while stack:
+#         vertex = stack.pop()
+#         if vertex not in visited:
+#             visited.append(vertex)
+#             for x in graph[vertex]:
+#                 if x not in visited:
+#                     stack.extend(x)
+#     return visited
+
+
+# def bfs(graph, start):
+#     queue = [[start]]
+#     visited = set()
+#     while queue:
+#         path = queue.pop(0)
+#         vertex = path[-1]
+#         if vertex not in visited:
+#             for current_neighbour in graph.get(vertex, []):
+#                 new_path = list(path)
+#                 new_path.append(current_neighbour)
+#                 queue.append(new_path)
+#             visited.add(vertex)
+
+
+
+
+
 def checkio(teleports_string):
     route = ['1']
     node = dict()
     for i in teleports_string.split(','):
         node.setdefault(i[0], []).append(i[1]), node.setdefault(i[1], []).append(i[0])
     print node
-
     pre = 0
-    current = route[-1]
-    open2 = []
-    for x in node[current]:
-        print x
-        if x > pre:
-            open2.append(x)
-    print open2
+    while len(set(route)) != len(node) or route[-1] != '1':
+        current = route[-1]
+        open = [x for x in node[current] if x > pre]
+        if not open:
+            route.pop(-1)
+            pre = current
+            node[pre].append(route[-1])
+            node[route[-1]].append(pre)
 
+        else:
+            m = min(open)
+            route.append(m)
+            node[m].remove(current)
+            node[current].remove(m)
+            pre = 0
 
+    return ''.join(route)
 
 
 
@@ -41,7 +78,7 @@ if __name__ == "__main__":
                 return False
         return True
 
-    assert check_solution(checkio, "12,23,34,45,56,67,78,81"), "First"
-    #assert check_solution(checkio, "12,28,87,71,13,14,34,35,45,46,63,65"), "Second"
+    #assert check_solution(checkio, "12,23,34,45,56,67,78,81"), "First"
+    assert check_solution(checkio, "12,28,87,71,13,14,34,35,45,46,63,65"), "Second"
     #assert check_solution(checkio, "12,15,16,23,24,28,83,85,86,87,71,74,56"), "Third"
     #assert check_solution(checkio, "13,14,23,25,34,35,47,56,58,76,68"), "Fourth"
