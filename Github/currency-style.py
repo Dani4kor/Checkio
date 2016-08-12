a8c5fa88cfc434d473e1ec3e437f3a2ffe4c5147
Dani <Dani4kor@gmail.com>
2016-08-12 22:56:47 +0300

    add currency-style template

--- Github/currency-style.py
+++ Github/currency-style.py
@@ -1,19 +1,34 @@
 def checkio(text):
-    print text
-    print text.index('$',14)
-    print text.split(' ', )
+    splitl = text.split(' ', )
+    for x in splitl:
+        if x[0] == '$':
+            if x[-1] is ',' or  '.':
+                x, nonitem = x[:-1], x[-1]
+
+            countdot = x.count(".")
+            countdot = countdot + x.count(",")
+            if countdot < 2:
+                splitdot = x.replace(',','.')
+                print splitdot
+            else:
+                splitdot = x.replace(',','.')
+                print splitdot
+            #print splitdot
 
 
 
 
-    return ""
+
+
+
+
 
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
     #assert checkio("$1.234.567,89") == "$1,234,567.89", "1st Example"
     #assert checkio("$0,89") == "$0.89", "2nd Example"
-    assert checkio("Euro Style = $12.345,67, US Style = $12,345.67") == \
+    assert checkio("Euro Style = $12.345,67, $12.12 US Style = $12,345.67") == \
            "Euro Style = $12,345.67, US Style = $12,345.67", "European and US"
     assert checkio("Us Style = $12,345.67, Euro Style = $12.345,67") == \
            "Us Style = $12,345.67, Euro Style = $12,345.67", "US and European"
