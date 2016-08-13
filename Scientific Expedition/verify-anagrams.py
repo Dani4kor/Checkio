05d4a1320134e187419641c4808574d48838f3e6
Dani <Dani4kor@gmail.com>
2016-08-13 08:46:51 +0300

    add solution for verify-anagrams

---
+++ Scientific Expedition/verify-anagrams.py
@@ -0,0 +1,11 @@
+def verify_anagrams(first_word, second_word):
+    verify = lambda x: ''.join(sorted(x.lower().replace(' ', '')))
+    return verify(first_word) == verify(second_word)
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(verify_anagrams(u"a", u"z"), bool), "Boolean!"
+    assert verify_anagrams(u"Programming", u"Gram Ring Mop") == True, "Gram of code"
+    assert verify_anagrams(u"Hello", u"Ole Oh") == False, "Hello! Ole Oh!"
+    assert verify_anagrams(u"Kyoto", u"Tokyo") == True, "The global warming crisis of 3002"
