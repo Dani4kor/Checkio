cd083f8fa02a570b4cf96484b8170b99034575f2
Dani <Dani4kor@gmail.com>
2016-08-13 06:36:25 +0300

    solution without cents convert, nned to be fixed

--- Github/currency-style.py
+++ Github/currency-style.py
@@ -1,19 +1,26 @@
 def checkio(text):
-    splitl = text.split(' ', )
-    for x in splitl:
-        if x[0] == '$':
-            if x[-1] is ',' or  '.':
-                x, nonitem = x[:-1], x[-1]
 
+    l = text.split(' ', )
+    print l
+    for x in l:
+        if x[0] == '$':
+            splitdot = ""
+            nonitem = ""
+            indexitem = l.index(x)
+            l.remove(x)
+            if x[-1] == ',' or x[-1] == '.':
+                x, nonitem = x[:-1], x[-1]
             countdot = x.count(".")
             countdot = countdot + x.count(",")
             if countdot < 2:
                 splitdot = x.replace(',','.')
-                print splitdot
             else:
                 splitdot = x.replace(',','.')
-                print splitdot
-            #print splitdot
+                splitdot = splitdot.replace('.', ',', (splitdot.count('.')-1))
+            if nonitem == ',' or nonitem == '.':
+                splitdot = splitdot + nonitem
+            l.insert(indexitem,splitdot)
+    print ' '.join(l)
 
 
 
@@ -26,11 +33,11 @@ def checkio(text):
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
-    #assert checkio("$1.234.567,89") == "$1,234,567.89", "1st Example"
-    #assert checkio("$0,89") == "$0.89", "2nd Example"
-    assert checkio("Euro Style = $12.345,67, $12.12 US Style = $12,345.67") == \
-           "Euro Style = $12,345.67, US Style = $12,345.67", "European and US"
-    assert checkio("Us Style = $12,345.67, Euro Style = $12.345,67") == \
-           "Us Style = $12,345.67, Euro Style = $12,345.67", "US and European"
+    # assert checkio("$1.234.567,89") == "$1,234,567.89", "1st Example"
+    # assert checkio("$0,89") == "$0.89", "2nd Example"
+    # assert checkio("Euro Style = $12.345,67, US Style = $12,345.67") == \
+    #        "Euro Style = $12,345.67, US Style = $12,345.67", "European and US"
+    # assert checkio("Us Style = $12,345.67, Euro Style = $12.345,67") == \
+    #        "Us Style = $12,345.67, Euro Style = $12,345.67", "US and European"
     assert checkio("$1.234, $5.678 and $9") == \
            "$1,234, $5,678 and $9", "Dollars without cents"
