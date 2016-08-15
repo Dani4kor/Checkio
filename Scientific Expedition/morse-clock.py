aef99bbc60e4cbff5d06c3832b7b3626bd083b0d
Dani <Dani4kor@gmail.com>
2016-08-15 06:48:08 +0300

    morse-clock template

---
+++ Scientific Expedition/morse-clock.py
@@ -0,0 +1,17 @@
+def checkio(time_string):
+
+    h,m,s = time_string.split(':', )
+
+    print bin(int(h))[2:]
+
+
+    #print bin(digit)[2:2 + width].zfill(width).replace('0', '.').replace('1', '-')
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(u"10:37:49") == ".- .... : .-- .--- : -.. -..-", "First Test"
+    assert checkio(u"21:34:56") == "-. ...- : .-- .-.. : -.- .--.", "Second Test"
+    assert checkio(u"00:1:02") == ".. .... : ... ...- : ... ..-.", "Third Test"
+    assert checkio(u"23:59:59") == "-. ..-- : -.- -..- : -.- -..-", "Fourth Test"
+
